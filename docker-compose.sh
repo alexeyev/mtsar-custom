d966b970d659fc98eeed20d4d0184a8e9e898f2c
Anton <anton.m.alexeyev@gmail.com>
2024-01-19 00:46:34 +0300

    docker-compose modifications

---
+++ docker-compose.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp docker-compose.yml.example docker-compose.yml
+docker-compose build
+docker-compose up -d postgres
+#docker-compose run --rm app /mtsar/mtsar.sh db migrate
+docker-compose up --no-recreate -d app
