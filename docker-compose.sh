6b912d1dd7050bdab0862f3cd4f3183c1276543c
Anton Alekseev <anton.m.alexeyev@gmail.com>
2024-01-19 01:04:42 +0300

    uncomment

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,5 +3,5 @@
 cp docker-compose.yml.example docker-compose.yml
 docker-compose build
 docker-compose up -d postgres
-#docker-compose run --rm app /mtsar/mtsar.sh db migrate
+docker-compose run --rm app /mtsar/mtsar.sh db migrate
 docker-compose up --no-recreate -d app
